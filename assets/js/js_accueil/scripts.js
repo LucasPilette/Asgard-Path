c5072ee9c7a751a655090af5380f7367362f765a
Lucas Pilette <pilettelucas@gmail.com>
2022-02-11 16:28:35 +0100

    responsive

--- assets/js/js_accueil/scripts.js
+++ assets/js/js_accueil/scripts.js
@@ -4,37 +4,38 @@ const container = document.querySelector('.containerTop');
 const header = document.querySelector('header')
 const boxes = document.querySelectorAll('.box');
 const logo = document.getElementById('logo');
-const connection = document.getElementById('connection');
+const connection = document.querySelectorAll('.connection');
 const modal = document.getElementById('modal');
 const closeModal = document.getElementById('close');
+const burgersvg = document.getElementById('burgersvg');
+const listNavMobile = document.querySelector('.listNavMobile')
 
-
-window.addEventListener('scroll', ()=> {
+window.addEventListener('scroll', () => {
     let scrollPos = window.scrollY;
-    if(scrollPos >=100){
+    if (scrollPos >= 100) {
         headerEffect.className = 'blurEffect';
     } else {
-        headerEffect.className ='invisEffect';
+        headerEffect.className = 'invisEffect';
     }
 })
 
-window.addEventListener('scroll', ()=> {
+window.addEventListener('scroll', () => {
     let scrollPos = window.scrollY;
-    let containerHeight = (container.offsetHeight) 
-    let headerHeight = (header.offsetHeight) 
+    let containerHeight = (container.offsetHeight)
+    let headerHeight = (header.offsetHeight)
     console.log(containerHeight)
-    if(scrollPos >= (containerHeight-headerHeight)){
+    if (scrollPos >= (containerHeight - headerHeight)) {
         navList.classList.add('navListWhite');
     } else {
         navList.classList.remove('navListWhite');
     }
 })
 
-document.querySelectorAll('li[href^="#"]').forEach(elem =>{
+document.querySelectorAll('li[href^="#"]').forEach(elem => {
     elem.addEventListener("click", e => {
         e.preventDefault();
         document.querySelector(elem.getAttribute('href')).scrollIntoView({
-            behavior:"smooth",
+            behavior: "smooth",
             offsetTop: 50
         });
     });
@@ -42,18 +43,20 @@ document.querySelectorAll('li[href^="#"]').forEach(elem =>{
 
 logo.addEventListener('click', () => {
     document.getElementById('home').scrollIntoView({
-        behavior:"smooth",
+        behavior: "smooth",
         offsetTop: 50
     });
 })
 
-
-connection.addEventListener('click', ()=> {
-    modal.style.display='block';
+connection.forEach( elem => {
+    elem.addEventListener('click', () => {
+        modal.style.display = 'block';
+    })
 })
 
-closeModal.addEventListener('click', ()=>{
-    modal.style.display='none';
+
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
 })
 
 
@@ -62,3 +65,13 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
+
+
+
+burgersvg.addEventListener('click',()=> {
+    if (listNavMobile.style.display === "block"){
+        listNavMobile.style.display = "none";
+    } else {
+        listNavMobile.style.display = "block";
+    }
+})
